d1fff665576a00a3415a1969bd6730f3f830baa8
K-kibet <charleykibet254@gmail.com>
2022-09-18 19:09:10 +0300

    first commit

---
+++ precache-manifest.1ea5329819d60d05e4ad3b98554d461a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bee7cffcdf1b3e7065cb",
+    "url": "/react-football/static/css/main.b57b0a86.chunk.css"
+  },
+  {
+    "revision": "bee7cffcdf1b3e7065cb",
+    "url": "/react-football/static/js/main.9638b800.chunk.js"
+  },
+  {
+    "revision": "a4ca96e97b4025695c65",
+    "url": "/react-football/static/js/runtime~main.5ec31da4.js"
+  },
+  {
+    "revision": "810dc173ae0421ed4e32",
+    "url": "/react-football/static/js/2.c9eef5a5.chunk.js"
+  },
+  {
+    "revision": "2be242902f8f7daeaf6ff1f729b60ad1",
+    "url": "/react-football/static/media/LaLiga.2be24290.png"
+  },
+  {
+    "revision": "3e055fb491632cd7be494535d08a13e1",
+    "url": "/react-football/static/media/Bundesliga.3e055fb4.png"
+  },
+  {
+    "revision": "0723988637eb0bcceb4564d8647b8599",
+    "url": "/react-football/static/media/Eredivisie.07239886.png"
+  },
+  {
+    "revision": "4b2af31f18df85fc286831937615225b",
+    "url": "/react-football/static/media/Ligue1.4b2af31f.png"
+  },
+  {
+    "revision": "2ea36f1c21b081213f172410bba0355b",
+    "url": "/react-football/static/media/PremierLeague.2ea36f1c.png"
+  },
+  {
+    "revision": "3ee74dc22ac21026487a24b49cf2ccfa",
+    "url": "/react-football/static/media/SerieA.3ee74dc2.png"
+  },
+  {
+    "revision": "2926e3234bdf7944fb2288662353c313",
+    "url": "/react-football/index.html"
+  }
+];
